41276cd07e3075355deb483a8be7584b75f33764
Marina Bonin <166030097+marinabonin@users.noreply.github.com>
2024-09-25 14:10:28 -0300

    Create pages structure (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import "./App.css";
-import Button from "./components/Button/Button";
-import Card from "./components/Card/Card";
+import Promotion from "./pages/Promotion";
 
 function App() {
-  function handleClick() {
-    alert("Checked out!");
-  }
-  return (
-    <div className="page">
-      <div className="voucher">
-        <h1 className="page-title">Voucher Promo</h1>
-        <Card
-          title="Special Deal For
-December"
-          category="icecream"
-        />
-        <Card
-          title="Special Deal For
-January"
-        />
-      </div>
-      <div className="actions">
-        <Button onClick={handleClick}>Check Out</Button>
-      </div>
-    </div>
-  );
+  return <Promotion />;
 }
 
 export default App;
