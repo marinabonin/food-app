e3cd3e0a5b1d3697f72ffc6203c6b5cb6ff4e8d0
Marina Bonin <marinacbonin@gmail.com>
2024-09-26 10:59:16 -0300

    Create menu bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import FoodCard from "./components/FoodCard/FoodCard";
 // import SearchBar from "./components/SearchBar/SearchBar";
-
-import burgerImg from "./components/FoodCard/food-card-burger.png";
+// import FoodCard from "./components/FoodCard/FoodCard";
+// import burgerImg from "./components/FoodCard/food-card-burger.png";
+import MenuBar from "./components/MenuBar/MenuBar";
 
 function App() {
-  return <FoodCard src={burgerImg} title="Zinger Burger" price="$2" />;
+  return <MenuBar />;
+  // <SearchBar />
+  // <FoodCard src={burgerImg} title="Zinger Burger" price="$2" />;
 }
 
 export default App;
