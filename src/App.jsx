908582fc7615f74e4d015a8b6427934776803163
Marina Bonin <marinacbonin@gmail.com>
2024-10-09 10:22:35 -0300

    Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.css";
-import FormLogin from "./pages/FormLogin/FormLogin";
+import FormLogin from "./components/FormLogin/FormLogin";
 
 function App() {
-  return <FormLogin />;
+  return (
+    <>
+      <img className="page-logo" src="/page-logo.png" />
+      <h1 className="page-title">Deliver Favourite Food</h1>
+      <div className="form-login">
+        <FormLogin />
+      </div>
+      <p className="account-text">Don't have an account?</p>
+      <a className="register-link" href="#">
+        REGISTER
+      </a>
+    </>
+  );
 }
 
 export default App;
