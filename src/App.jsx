385a3034ee5821416f6c52712ecc4f4b67761b6e
Marina Bonin <marinacbonin@gmail.com>
2024-10-11 09:38:56 -0300

    Create signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import "./App.css";
 import FormSignup from "./components/FormSignup/FormSignup";
 
 function App() {
-  return <FormSignup />;
+  return (
+    <>
+      <img className="page-logo" src="/page-logo.png" />
+      <h1 className="page-title">Deliver Favourite Food</h1>
+      <div className="form-login">
+        <FormSignup />
+      </div>
+      <p className="account-text">Already have an account?</p>
+      <a className="register-link" href="#">
+        Signup
+      </a>
+    </>
+  );
 }
 
 export default App;
