863e0baa0d0791dab4653216f9e61debbbad4b8b
Marina Bonin <marinacbonin@gmail.com>
2024-10-11 10:52:14 -0300

    Create page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.css";
-import FormSignup from "./components/FormSignup/FormSignup";
 
 function App() {
-  return (
-    <>
-      <img className="page-logo" src="/page-logo.png" />
-      <h1 className="page-title">Deliver Favourite Food</h1>
-      <div className="form-login">
-        <FormSignup />
-      </div>
-      <p className="account-text">Already have an account?</p>
-      <a className="register-link" href="#">
-        Signup
-      </a>
-    </>
-  );
+  return;
 }
 
 export default App;
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,18 @@
+import "./Signup.css";
+import FormSignup from "./components/FormSignup/FormSignup";
+
+export default function Signup() {
+  return (
+    <>
+      <img className="page-logo" src="/page-logo.png" />
+      <h1 className="page-title">Deliver Favourite Food</h1>
+      <div className="form-login">
+        <FormSignup />
+      </div>
+      <p className="account-text">Already have an account?</p>
+      <a className="register-link" href="#">
+        Signup
+      </a>
+    </>
+  );
+}
