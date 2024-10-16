c55771e5b06498bc32edc508252bb26972c0e4af
Marina Bonin <marinacbonin@gmail.com>
2024-10-16 09:59:08 -0300

    Integrate with fake backend using fetch

--- src/pages/ExploreMenu/ExploreMenu.jsx
+++ src/pages/ExploreMenu/ExploreMenu.jsx
@@ -2,15 +2,20 @@ import "./ExploreMenu.css";
 import FoodCard from "../../components/FoodCard/FoodCard";
 import MenuBar from "../../components/MenuBar/MenuBar";
 import SearchBar from "../../components/SearchBar/SearchBar";
-// import zburgerImg from "../../components/FoodCard/food-card-zburger.png";
-// import rollImg from "../../components/FoodCard/food-card-roll.png";
-// import burgerImg from "../../components/FoodCard/food-card-burger.png";
-// import sandwichImg from "../../components/FoodCard/food-card-sandwich.png";
-// import pizzaImg from "../../components/FoodCard/food-card-pizza-roll.png";
-// import misoImg from "../../components/FoodCard/food-card-miso-soup.png";
-import menuItems from "./menu-items.json";
+import { useEffect, useState } from "react";
 
 export default function ExploreMenu() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    async function getMenuItems() {
+      const response = await fetch("http://localhost:5173/menu-items.json");
+      const data = await response.json();
+      setMenuItems(data);
+    }
+    getMenuItems();
+  }, []);
+
   return (
     <div className="container">
       <h1 className="page-title">
@@ -19,6 +24,7 @@ export default function ExploreMenu() {
       <SearchBar />
       <h2 className="food-cards-title">Popular Menu</h2>
       <div className="food-cards">
+        {console.log(menuItems)}
         {menuItems.map((item) => {
           return (
             <FoodCard
@@ -29,17 +35,6 @@ export default function ExploreMenu() {
             />
           );
         })}
-        {/* <FoodCard src={zburgerImg} title="Z Burger" price="$2" />
-        <FoodCard src={rollImg} title="Roll Paratha" price="$3" />
-        <FoodCard src={burgerImg} title="Burger" price="$3" />
-        <FoodCard src={sandwichImg} title="Sandwich" price="$2" />
-        <FoodCard src={pizzaImg} title="Pizza Roll" price="$3" />
-        <FoodCard src={misoImg} title="Miso Soup" price="$2" />
-        <FoodCard src={pizzaImg} title="Pizza Roll" price="$3" />
-        <FoodCard src={misoImg} title="Miso Soup" price="$2" />
-        <FoodCard src={pizzaImg} title="Pizza Roll" price="$3" />
-        <FoodCard src={misoImg} title="Miso Soup" price="$2" />
-        <FoodCard src={pizzaImg} title="Pizza Roll" price="$3" /> */}
       </div>
       <div className="menubar">
         <MenuBar />
